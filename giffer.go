0b7c104b9e649b9f00f73678a5753d537b295155
AzinKhan <azink91@googlemail.com>
2018-10-16 21:45:20 +0100

    Parallelize conversion to GIF

--- giffer.go
+++ giffer.go
@@ -6,14 +6,17 @@ import (
 	"image/gif"
 	"image/jpeg"
 	"log"
+	"sync"
 )
 
 func decodeJPG(data []byte) (image.Image, error) {
+	log.Println("Decoding JPEG")
 	img, err := jpeg.Decode(bytes.NewReader(data))
 	return img, err
 }
 
 func ConvertToGIF(img image.Image) (*image.Paletted, error) {
+	log.Println("Encoding to GIF")
 	var b []byte
 	bf := bytes.NewBuffer(b)
 	var opt gif.Options
@@ -27,6 +30,23 @@ func ConvertToGIF(img image.Image) (*image.Paletted, error) {
 	return im.(*image.Paletted), err
 }
 
+var wg sync.WaitGroup
+
+func Convert(data []byte) (*image.Paletted, error) {
+	img, err := decodeJPG(data)
+	if err != nil {
+		log.Printf("Error decoding JPEG: %+v", err)
+		return nil, err
+	}
+	GIF, err := ConvertToGIF(img)
+	if err != nil {
+		log.Printf("Error converting to GIF: %+v", err)
+		return nil, err
+	}
+	return GIF, nil
+
+}
+
 func Giffer(inputData [][]byte) (*bytes.Buffer, error) {
 	G := &gif.GIF{
 		LoopCount: 0,
@@ -35,17 +55,26 @@ func Giffer(inputData [][]byte) (*bytes.Buffer, error) {
 		Image:     make([]*image.Paletted, len(inputData)),
 	}
 	log.Println("Converting images to GIF")
-	for i, data := range inputData {
-		img, err := decodeJPG(data)
+	errChan := make(chan error, len(inputData))
+	wg.Add(len(inputData))
+	for index, d := range inputData {
+		data := d
+		i := index
+		go func() {
+			defer wg.Done()
+			GIF, err := Convert(data)
+			errChan <- err
+			G.Delay[i] = 8
+			G.Image[i] = GIF
+		}()
+	}
+	wg.Wait()
+	// Don't forget to close the channel!
+	close(errChan)
+	for err := range errChan {
 		if err != nil {
 			return nil, err
 		}
-		GIF, err := ConvertToGIF(img)
-		if err != nil {
-			return nil, err
-		}
-		G.Image[i] = GIF
-		G.Delay[i] = 8
 	}
 	log.Printf("Encoding %+v images into GIF", len(G.Image))
 	var buf []byte
